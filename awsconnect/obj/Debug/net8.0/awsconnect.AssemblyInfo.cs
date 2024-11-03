466187f7ab16dee414e0a4ec2cd06cda14595c0f
Adam-WY-Jeong <jwy0805@gmail.com>
2024-11-03 13:29:21 +0900

    aws conf

--- awsconnect/obj/Debug/net8.0/awsconnect.AssemblyInfo.cs
+++ awsconnect/obj/Debug/net8.0/awsconnect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("awsconnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d643c447eb40ee73e8c54dfeed2145881eb946c")]
 [assembly: System.Reflection.AssemblyProductAttribute("awsconnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("awsconnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
